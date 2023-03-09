a8a14052b56c3b663f36ab325dfb886d7d7a471a
Resma Adi Nugroho <resmaadi@gmail.com>
2023-03-09 19:47:49 +0700

    added validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
 var img = document.getElementById('img');
-
 var slides=['src/img/image.png','src/img/image2.png','src/img/image3.png'];
-
 var Start=0;
 
 function slider(){
@@ -16,3 +14,36 @@ function slider(){
    
 }
 setInterval(slider,4000);
+
+
+const form = document.getElementById('contact-form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const destination = document.getElementById('destination');
+
+form.addEventListener('submit', function(event) {
+  if (!validateName(name.value)) {
+    event.preventDefault();
+    alert('Nama tidak valid');
+  }
+
+  if (!validateEmail(email.value)) {
+    event.preventDefault();
+    alert('Email tidak valid');
+  }
+
+  if (destination.value === '') {
+    event.preventDefault();
+    alert('Harap pilih destinasi wisata');
+  }
+});
+
+function validateName(name) {
+  const regex = /^[a-zA-Z ]+$/;
+  return regex.test(name);
+}
+
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
